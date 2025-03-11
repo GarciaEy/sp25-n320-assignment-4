6853ac870e11bdf2d2cbee706ca7b9bf57b23d70
charleycorneil <146792079+charleycorneil@users.noreply.github.com>
2025-03-10 23:38:14 -0400

    certificate changes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,15 @@ const WebFile = require("./functions/webfile");
  * @param {http.ServerResponse} res
  */
 
+if (req.method === "GET" && req.url === "/course-certificate") {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(
+    fs.readFileSync(path.join(__dirname, "views", "course-certificate.html"))
+  );
+  res.end();
+  return;
+}
+
 if (req.method === "GET" && req.url === "/invoice") {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write(fs.readFileSync(path.join(__dirname, "views", "invoice.html")));
@@ -21,6 +30,32 @@ function app(req, res) {
   if (req.method === "GET" && !req.url.startsWith("/api")) {
     const fileReq = new WebFile(req.url);
     const filePath = path.join(__dirname, "views", fileReq.filename);
+
+    if (
+      req.url.startsWith("/css/") ||
+      req.url.startsWith("/js/") ||
+      req.url.startsWith("/images/")
+    ) {
+      const filePathStatic = path.join(__dirname, req.url);
+      if (fs.existsSync(filePathStatic)) {
+        const ext = path.extname(filePathStatic);
+        const contentType =
+          ext === ".css"
+            ? "text/css"
+            : ext === ".js"
+            ? "application/javascript"
+            : ext === ".jpg" || ext === ".jpeg"
+            ? "image/jpeg"
+            : ext === ".png"
+            ? "image/png"
+            : "text/plain";
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.write(fs.readFileSync(filePathStatic));
+        res.end();
+        return;
+      }
+    }
 
     const contentType =
       fs.existsSync(filePath) && fileReq.getExtension()
@@ -56,7 +91,7 @@ function app(req, res) {
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 5445; // 3000, 3001,
+const port = process.env.PORT || 5445;
 
 server.listen(port);
 
